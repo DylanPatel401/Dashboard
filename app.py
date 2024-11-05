df3ba871500d302ffbf4e2491c2a83b20a21f96a
Dylan Patel <PatelDylan401@gmail.com>
2024-11-04 19:37:33 -0500

    port

--- app.py
+++ app.py
@@ -1,7 +1,14 @@
-from flask import Flask, render_template
+from flask import Flask
+import os
 
 app = Flask(__name__)
 
-@app.route("/")
+@app.route('/')
 def home():
-    return render_template("index.html")
+    return "Welcome to my Flask app!"
+
+if __name__ == '__main__':
+    # Get the port from the environment variable, default to 5000 if not set
+    port = int(os.environ.get('PORT', 5000))
+    # Run the Flask app
+    app.run(host='0.0.0.0', port=port)
